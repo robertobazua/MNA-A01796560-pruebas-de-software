9b045b4939f531a59ad8d549b095d4c0d5875e09
robertobazua <44980195+robertobazua@users.noreply.github.com>
2026-02-15 03:29:39 +0000

    feat: Se crea funcion para leer los archivos JSON con manejo de excepciones

--- 5.2/P1/source/computeSales.py
+++ 5.2/P1/source/computeSales.py
@@ -1,4 +1,16 @@
 import sys
+import json
+
+def cargar_archivo(ruta_archivo):
+    try:
+        with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+            return json.load(archivo)
+    except FileNotFoundError:
+        print(f"Error: El archivo {ruta_archivo} no fue encontrado.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: El archivo {ruta_archivo} no tiene un formato JSON valido.")
+        return None
 
 def main():
     if len(sys.argv) != 3:
