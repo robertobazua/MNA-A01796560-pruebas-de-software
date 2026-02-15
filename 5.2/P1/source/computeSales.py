175302023f5da2d813bd575e9c53c11e29524a23
robertobazua <44980195+robertobazua@users.noreply.github.com>
2026-02-15 04:01:26 +0000

    test: Se hizo pruebo para comprobar la carga de archivos

--- 5.2/P1/source/computeSales.py
+++ 5.2/P1/source/computeSales.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+import time
 
 def cargar_archivo(ruta_archivo):
     try:
@@ -13,13 +14,24 @@ def cargar_archivo(ruta_archivo):
         return None
 
 def main():
+
+    inicia_tiempo = time.time()
+
     if len(sys.argv) != 3:
         print("Usar: python computeSales.py priceCatalogue.json salesRecord.json")
         sys.exit(1)
     
-    catalogo_precios = sys.argv[1]
-    registro_ventas = sys.argv[2]
+    ruta = ""
+    catalogo_precios = ruta + sys.argv[1]
+    registro_ventas = ruta + sys.argv[2]
     print(f"Archivos: {catalogo_precios}, {registro_ventas}")
+
+    df_precios = cargar_archivo(catalogo_precios)
+    df_ventas = cargar_archivo(registro_ventas)
+
+    tiempo_ejecucion = time.time() - inicia_tiempo
+
+    print(f"Tiempo de ejecucion: {tiempo_ejecucion:.4f} segundos")
 
 if __name__ == "__main__":
     main()
