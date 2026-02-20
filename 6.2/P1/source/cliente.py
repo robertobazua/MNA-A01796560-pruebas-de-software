bdc55d223a9b47d40ee3e81498927b68077a6373
robertobazua <44980195+robertobazua@users.noreply.github.com>
2026-02-20 21:58:14 +0000

    refactor: Se atienden observaciones de pylint para cliente.py

--- 6.2/P1/source/cliente.py
+++ 6.2/P1/source/cliente.py
@@ -11,36 +11,37 @@ class Cliente:
     """Clase Cliente"""
 
     @classmethod
-    def crear_cliente(cls, id, nombre, email):
+    def crear_cliente(cls, id_cliente, nombre, email):
+        """Crear cliente en la BD"""
         df = cargar_dataframe(ARCHIVO, COLUMNAS)
-        if id in df["id"].values:
-            print(f"Error: Cliente {id} ya existe.")
+        if id_cliente in df["id"].values:
+            print(f"Error: Cliente {id_cliente} ya existe.")
             return False
-        nuevo_reg = pd.DataFrame([[id, nombre, email]], columns=COLUMNAS)
+        nuevo_reg = pd.DataFrame([[id_cliente, nombre, email]], columns=COLUMNAS)
         df = pd.concat([df, nuevo_reg], ignore_index=True)
         guardar_dataframe(df, ARCHIVO)
         return True
 
     @classmethod
-    def eliminar_cliente(cls, id):
+    def eliminar_cliente(cls, id_cliente):
         """Elimina un cliente por ID"""
         df = cargar_dataframe(ARCHIVO, COLUMNAS)
-        if id not in df["id"].values:
-            print(f"Error: Cliente {id} no encontrado.")
+        if id_cliente not in df["id"].values:
+            print(f"Error: Cliente {id_cliente} no encontrado.")
             return False
-        df = df[df["id"] != id]
+        df = df[df["id_cliente"] != id_cliente]
         guardar_dataframe(df, ARCHIVO)
         return True
 
     @classmethod
-    def listar_clientes(cls):
+    def listar_clientes(cls, id_cliente):
         """Lista todos los clientes"""
 
         df = cargar_dataframe(ARCHIVO, COLUMNAS)
-        row = df[df["id"] == id]
+        row = df[df["id_cliente"] == id_cliente]
 
         if row.empty:
-            print(f"Error: Cliente {id} no encontrado.")
+            print(f"Error: Cliente {id_cliente} no encontrado.")
             return False
         print(row.to_string(index=False))
         return row
